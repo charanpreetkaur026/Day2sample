aff842a8cab2f8842e4c5c29730c41ad36db303f
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-10-09 19:21:50 -0400

    set and 2D arrays

--- Day2sample/main.swift
+++ Day2sample/main.swift
@@ -8,7 +8,65 @@
 
 import Foundation
 
-print("Hello, World!")
+var a:[Int]
+a  = [Int]()
+//print(a[0])  //error
+//a[0]=100
+//print(a[0])
 
-var str="aaabbcccdeebc"
-let count
+a.append(10)
+print(a[0])
+
+var b = Array.init(repeating: 0, count: 5)
+print(b[0])
+
+
+/*var c = Array.init(repeating: 0, count: 10)
+c.append(11)
+c.append(22)
+c.append(33)
+c.append(44)
+c.append(55)
+c.append(66)
+c.append(77)
+c.append(88)
+c.append(99)
+c.append(100)
+
+for i in c{
+    print(i)
+}
+c = c + b
+print(c)
+
+var twoD = [[Int]]()
+
+twoD.append([1,3,5])
+twoD.append([2,4,6])
+twoD.append([3,6,9])
+for row in  twoD{
+    print("")
+    for item in row{
+       print(twoD[[row]item])
+    }
+}*/
+
+var countryName : Set<String> = ["Canada", "USA", "UK", "INDIA"]
+print(countryName)
+countryName.insert("Australia")
+for country in countryName
+{
+print(country)
+}
+print("..............")
+for country in countryName.sorted()
+{
+    
+print(country)
+}
+
+var country2: Set<String> = ["Australia", "Rassia", "China"]
+print("..............")
+print(countryName.union(country2))
+
+
